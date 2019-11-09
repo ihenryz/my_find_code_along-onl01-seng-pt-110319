13a1a1a23f15cd7d62167e25ed312a0a9077aa24
Henry Phan <h.phan06@gmail.com>
2019-11-09 19:13:11 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,15 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-  collection[i] if yield(collection[i])
+  return collection[i] if yield(collection[i])
   i += 1 
   end
 end
+
+# def my_find(collection)
+#   i = 0
+#   while i < collection.length
+#     return collection[i] if yield(collection[i])
+#     i = i + 1
+#   end
+# end
