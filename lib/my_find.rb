72e5d17f4be9915c38841d8bf1ad63d8e8007c5e
Henry Phan <h.phan06@gmail.com>
2019-11-09 19:07:10 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,8 @@ require 'pry'
 def my_find(collection)
   counter = 0 
   while counter < collection.length 
-    return collection[counter] if yield(collection[counter])
+    collection[counter] if yield(collection[counter])
     end
   counter += 1 
-  end
 end
 
